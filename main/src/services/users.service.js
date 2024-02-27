ee4acd75f3d17c57428197492a2e5a562eea31cc
borislavpachev <borislav.pachev@gmail.com>
2024-02-27 17:00:56 +0200

    create user functionality added

--- main/src/services/users.service.js
+++ main/src/services/users.service.js
@@ -4,5 +4,17 @@ import { db, storage } from '../config/firebase-config';
 
 export const getUserData = async (uid) => {
 
-    return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
-  };
+  return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+};
+
+export const getUserByUsername = async (username) => {
+
+  return get(ref(db, `users/${username}`));
+};
+
+export const createUser = async (username, firstName, lastName, email, uid, phoneNumber, photoURL) => {
+
+  return set(ref(db, `users/${username}`), { username, firstName, lastName, uid, email, createdOn: Date.now(), phoneNumber, photoURL })
+};
+
+
