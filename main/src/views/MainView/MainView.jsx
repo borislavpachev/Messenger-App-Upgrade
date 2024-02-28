6d11d3117c5183d53c94d471d2ad1d6c4caf02ef
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-02-28 16:33:43 +0200

    grid-basics-changes

--- main/src/views/MainView/MainView.jsx
+++ main/src/views/MainView/MainView.jsx
@@ -2,6 +2,10 @@ import { AppContext } from "../../context/AppContext";
 import { useContext } from "react";
 import { logoutUser } from "../../services/auth.service";
 import { NavLink, useNavigate } from 'react-router-dom';
+import TeamBar from "../../components/TeamBar/TeamBar";
+import ChannelBar from "../../components/ChannelBar/ChannelBar";
+import Header from "../../components/Header/Header";
+import ContentBox from "../../components/ContentBox/ContentBox";
 
 export default function MainView() {
     const { user, userData, setAppState } = useContext(AppContext);
@@ -15,12 +19,29 @@ export default function MainView() {
     }
 
     return (
-        <div>
-            <h2>main view</h2>
-            <div>
-                <NavLink to="/user-profile">Profile</NavLink>
-            </div>
-            <button onClick={logout}>Logout</button>
+        <div className="container-fluid h-100 m-2 p-0">
+        <div className="row h-100">          
+            <TeamBar />         
+            <ChannelBar/>          
+          <div className="col-9 d-flex flex-column">
+            <Header />
+            <ContentBox className="flex-grow-1 bg-dark" />            
+          </div>          
         </div>
-    )
+      </div>
+    );
+    
+
 }
+
+
+
+// return (
+//     <div>
+//         <h2>main view</h2>
+//         <div>
+//             <NavLink to="/user-profile">Profile</NavLink>
+//         </div>
+//         <button onClick={logout}>Logout</button>
+//     </div>
+// )
