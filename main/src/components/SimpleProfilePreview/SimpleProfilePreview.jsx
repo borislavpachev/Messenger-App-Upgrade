ded2f9df5440143ec1759255518d472bd9995e25
borislavpachev <borislav.pachev@gmail.com>
2024-02-28 21:28:04 +0200

    simple profile preview added

---
+++ main/src/components/SimpleProfilePreview/SimpleProfilePreview.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import { getUserDataByUsername } from "../../services/users.service";
+import { CgProfile } from "react-icons/cg";
+import './SimpleProfilePreview.css';
+
+export default function SimpleProfilePreview({ username }) {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        getUserDataByUsername(username).then(setCurrentUser)
+    }, []);
+
+    return (
+        <>
+            {
+                currentUser ?
+                    (
+                        <div className="container bg-primary">
+
+                            {(!currentUser.photoURL) ?
+                                <CgProfile className="rounded-circle custom-img" />
+                                :
+                                <img alt="avatar-mini" className="rounded-circle custom-img " src={currentUser.photoURL} />
+                            }
+                            <span>{currentUser.firstName} {currentUser.lastName}</span>
+                        </div >
+                    ) : null
+            }
+        </>
+    )
+}
+
+SimpleProfilePreview.propTypes = {
+    username: PropTypes.string,
+}
