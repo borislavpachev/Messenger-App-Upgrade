0ca21891c476b28467dc03a30dd9e2bf7823f852
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-02-29 20:59:56 +0200

    channel-bar-create

---
+++ main/src/components/MainBar/MainBar.jsx
@@ -0,0 +1,9 @@
+import './MainBar.css'
+import ChannelBar from '../ChannelBar/ChannelBar'
+export default function MainBar(){
+    return (
+        <div className="col-2 h-100 main-bar-bg text-white">
+            <ChannelBar />
+      </div>
+    )
+}
