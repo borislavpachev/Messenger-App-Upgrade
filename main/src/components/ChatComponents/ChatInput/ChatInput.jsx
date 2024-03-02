47976aae111e68379e2081e4a28ab8fe5007b82d
borislavpachev <borislav.pachev@gmail.com>
2024-03-02 11:35:07 +0200

    chat input added

--- main/src/components/ChatComponents/ChatInput/ChatInput.jsx
+++ main/src/components/ChatComponents/ChatInput/ChatInput.jsx
@@ -1,15 +1,22 @@
 import { useContext, useState } from "react";
 import Button from "../../Button/Button";
+import PropTypes from 'prop-types';
 import { AppContext } from "../../../context/AppContext";
+import { sendMessage } from "../../../services/messages.service";
 
-export default function ChatInput() {
+export default function ChatInput({ chatId }) {
     const { userData } = useContext(AppContext);
     const [message, setMessage] = useState('');
 
-    const sendMessage = async () => {
-
+    const sendUserMessage = async () => {
+        try {
+            await sendMessage(chatId, userData.username, message);
+            setMessage('');
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-
+    
     const handleChange = (e) => {
         setMessage(e.target.value);
     }
@@ -18,9 +25,13 @@ export default function ChatInput() {
         <div>
             <form onSubmit={e => e.preventDefault()}>
                 <input type="text" name="message" id="message" value={message}
-                    onChange={handleChange} />
-                <Button type='submit'>send</Button>
+                    onChange={handleChange} style={{outline:'none', width: '90%', margin: '10px'}}/>
+                <Button type='submit' onClick={sendUserMessage}>send</Button>
             </form>
         </div>
     )
+}
+
+ChatInput.propTypes = {
+    chatId: PropTypes.string,
 }
