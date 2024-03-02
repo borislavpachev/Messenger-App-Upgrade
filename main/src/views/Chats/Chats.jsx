119e3419cf70358837ce93808b92c811dc66b5c4
borislavpachev <borislav.pachev@gmail.com>
2024-03-02 11:37:19 +0200

    chats view with it's components

--- main/src/views/Chats/Chats.jsx
+++ main/src/views/Chats/Chats.jsx
@@ -1,16 +1,32 @@
+import { useContext, useEffect, useState } from 'react';
 import ChatContent from '../../components/ChatComponents/ChatContent/ChatContent';
-import ChatInput from '../../components/ChatComponents/ChatInput/ChatInput';
 import CreateChatRoom from "../../components/ChatComponents/CreateChatRoom/CreateChatRoom";
 import UserChats from "../../components/ChatComponents/UserChats/UserChats";
+import { getChatsByParticipant } from '../../services/messages.service';
+import { AppContext } from '../../context/AppContext';
+import { useParams } from 'react-router-dom';
 
 export default function Chats() {
+    const { userData } = useContext(AppContext);
+    const [chats, setChats] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        getChatsByParticipant(userData.username).then(setChats)
+    }, []);
+
+    const onChatCreated = async () => {
+        const chats = await getChatsByParticipant(userData.username)
+        setChats(chats);
+    }
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <CreateChatRoom />
-            <UserChats />
-            {/* <ChatContent />
-            <ChatInput />  */}
+        <div className='chats-container'>
+            <CreateChatRoom onCreate={onChatCreated} />
+            <div className='chat-main'>
+            <UserChats chats={chats} />
+            <ChatContent chatId={id} />
+            </div>
         </div >
     )
 }
