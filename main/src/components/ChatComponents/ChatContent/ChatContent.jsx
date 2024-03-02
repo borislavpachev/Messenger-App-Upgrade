5417cf00740a6cff25f649a842a956660a7dc50a
borislavpachev <borislav.pachev@gmail.com>
2024-03-02 11:35:00 +0200

    chat content added

--- main/src/components/ChatComponents/ChatContent/ChatContent.jsx
+++ main/src/components/ChatComponents/ChatContent/ChatContent.jsx
@@ -1,7 +1,32 @@
-export default function ChatContent () {
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import { getChatById } from "../../../services/messages.service";
+import SimpleProfilePreview from "../../SimpleProfilePreview/SimpleProfilePreview";
+import ChatInput from "../ChatInput/ChatInput";
+
+export default function ChatContent({ chatId }) {
+    const [chat, setChat] = useState();
+
+    useEffect(() => {
+        getChatById(chatId).then(setChat)
+    }, [chatId]);
+
     return (
         <div className="chats-content">
-            CONTENT
+            {
+              chat ?  chat.map((message) => {
+                    return <div key={message.id}>
+                        <span><SimpleProfilePreview username={message.author}/> {new Date(message.sentOn).toLocaleString('bg-BG')}</span>
+                        <p>Message: {message.message}</p>
+                    </div>
+
+                }) : (' No messages yet.')
+            }
+            <ChatInput chatId={chatId} />
         </div>
     )
+}
+
+ChatContent.propTypes = {
+    chatId: PropTypes.string,
 }
