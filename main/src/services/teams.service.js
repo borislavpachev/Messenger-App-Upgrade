5a8ce2246694b1ed420088ce78d1dd21d59b1fec
DDankov <danko5656@gmail.com>
2024-03-03 14:43:54 +0200

    teams.service getTeamMembersByTeamId added, others fixed

--- main/src/services/teams.service.js
+++ main/src/services/teams.service.js
@@ -1,5 +1,5 @@
 import { db } from '../config/firebase-config';
-import { ref, get, set, push } from 'firebase/database';
+import { ref, get, set, push, onValue } from 'firebase/database';
 
 
 export const checkTeamNameExists = async (teamName) => {
@@ -58,14 +58,37 @@ export const createNewTeam = async (teamName, teamOwner, teamMembers, teamChanne
     return newTeamRef;
 };
 
-export const addUserToTeam = async (team, username) => {
-    return set(ref(db, `teams/${team.teamId}/teamMembers`), [...team.teamMembers, username]);
+export const addUserToTeam = async (teamId, username) => {
+  const teamMembers = await getTeamMembersByTeamId(teamId);
+  console.log(teamMembers);
+    return set(ref(db, `teams/${teamId}/teamMembers`), [...teamMembers, username]);
 };
 
-export const removeUserFromTeam = async (team, username) => {
-    return set(ref(db, `teams/${team.teamId}/teamMembers`), team.teamMembers.filter(member => member !== username));
+export const removeUserFromTeam = async (teamId, username) => {
+  const teamMembers = await getTeamMembersByTeamId(teamId);
+    return set(ref(db, `teams/${teamId}/teamMembers`), teamMembers.filter(member => member !== username));
 };
 
 export const getAllTeamMembers = async (team) => {
     return get(ref(db, `teams/${team.key}/teamMembers`));
 };
+
+
+export async function getTeamMembersByTeamId(teamId) {
+  const teamMembersRef = ref(db, `teams/${teamId}/teamMembers`);
+
+  return new Promise((resolve, reject) => {
+    onValue(teamMembersRef, (snapshot) => {
+      const data = snapshot.val();
+      if (!data) {
+        resolve([]);
+        return;
+      }
+
+      // If data is an array of team members, resolve it directly
+      resolve(data);
+    }, (error) => {
+      reject(error);
+    });
+  });
+}
