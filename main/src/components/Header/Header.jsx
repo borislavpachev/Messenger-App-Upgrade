f8ad68f5b5a6f8ab8e74b8457c652503378c79db
DDankov <danko5656@gmail.com>
2024-03-03 14:41:03 +0200

    Team Member List in Header

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-export default function Header(){
+import { useState } from 'react';
+import TeamMemberList from '../TeamMembersList/TeamMembersList';
+
+export default function Header( { teamId }){
+    const [showTeamMemberList, setShowTeamMemberList] = useState(false);
+
+    const handleOpenTeamMemberList = () => {
+      setShowTeamMemberList(true);
+    };
+
+    const handleCloseTeamMemberList = () => {
+      setShowTeamMemberList(false);
+    };
     return (
-        <header className="d-flex justify-content-between bg-dark text-white p-3">Header</header>
+        <header className="d-flex justify-content-between bg-dark text-white p-3">Header
+              <button onClick={handleOpenTeamMemberList}>Open Team Member List</button>
+       {showTeamMemberList && <TeamMemberList teamId={teamId} onClose={handleCloseTeamMemberList} />}
+        </header>
     )
 }
