d3966d1eb64feec44fde3e74d529065e58aa8914
DDankov <danko5656@gmail.com>
2024-03-06 20:02:41 +0200

    Header GeneralSearch included

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import TeamMemberList from '../TeamMembersList/TeamMembersList';
+import GeneralSearch from '../GeneralSearch/GeneralSearch'
 
 export default function Header( { teamId }){
     const [showTeamMemberList, setShowTeamMemberList] = useState(false);
@@ -12,8 +13,12 @@ export default function Header( { teamId }){
     const handleCloseTeamMemberList = () => {
       setShowTeamMemberList(false);
     };
+    
     return (
         <header className="d-flex justify-content-between bg-dark text-white p-3">Header
+        <div className='general-search-bar'>
+       <GeneralSearch teamId={teamId} />
+       </div>
         <div className='dropdown'>
         <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" >Team Members List</button>
        <TeamMemberList teamId={teamId} />
