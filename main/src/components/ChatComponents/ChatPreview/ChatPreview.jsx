e7d75fccec87cbb1af09687714f737ffa60f6a31
borislavpachev <borislav.pachev@gmail.com>
2024-03-06 13:22:20 +0200

    chat preview with recent message added

--- main/src/components/ChatComponents/ChatPreview/ChatPreview.jsx
+++ main/src/components/ChatComponents/ChatPreview/ChatPreview.jsx
@@ -1,24 +1,34 @@
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import { AppContext } from '../../../context/AppContext';
+import './ChatPreview.css'
 
-export default function ChatPreview({ users, chatId }) {
+export default function ChatPreview({ author, lastMessage, users, chatId }) {
     const { userData } = useContext(AppContext);
-
+  
     return (
         <NavLink to={`/chats/${chatId}`}>
-            <p style={{padding: '10px'}}>
+            <div style={{ padding: '10px', border: '1px solid black', margin: '10px' }}>
                 {
                     users
                         .filter((user) => user !== userData.username)
                         .join(', ')
-                }</p>
-        </NavLink>
+                }
+                {
+                    author ?
+                        (<p className='user-chats-last-message'>{author  === userData.username ? (<span>You</span>) : author }: {lastMessage}</p>) : (
+                            <p>Empty Chat</p>
+                        )
+                }
+            </div>
+        </NavLink >
     )
 }
 
 ChatPreview.propTypes = {
+    author: PropTypes.string,
+    lastMessage: PropTypes.string,
     users: PropTypes.array,
     chatId: PropTypes.string,
 }
