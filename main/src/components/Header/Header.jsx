266796e9e8ad96224033e3448fb18fe795a6776a
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-03-13 19:30:01 +0200

    leave channel add

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { get, ref} from 'firebase/database';
+import { useContext, useState } from 'react';
 import TeamMemberList from '../TeamMembersList/TeamMembersList';
 import GeneralSearch from '../GeneralSearch/GeneralSearch';
 import './Header.css';
+import { AppContext } from '../../context/AppContext';
+import { leaveChannel, getGeneralChannelId } from '../../services/channel.service';
+import { useParams, useNavigate } from 'react-router-dom';
+import toast from "react-hot-toast";
+import { db } from '../../config/firebase-config';
 
 
-export default function Header( { teamId }){
+
+export default function Header({ channelId }){
     // const [showTeamMemberList, setShowTeamMemberList] = useState(false);
 
     // const handleOpenTeamMemberList = () => {
@@ -15,16 +22,52 @@ export default function Header( { teamId }){
     // const handleCloseTeamMemberList = () => {
     //   setShowTeamMemberList(false);
     // };
-
+    const { teamId } = useParams()
+    const { userData } = useContext(AppContext)
     const [show, setShow] = useState(false);
+    const navigate = useNavigate();
 
     const toggleShow = () => {
       setShow(!show);
   }
 
+  const handleLeaveChannel = async () => {
+    try {
+      const generalChannelId = await getGeneralChannelId(teamId);
+      if (channelId === generalChannelId) {
+        toast.error("You cannot leave the general channel");
+        return;
+      }
+
+      const channelSnapshot = await get(ref(db, `channels/${channelId}`));
+      const channelData = channelSnapshot.val();
+  
+      // Check if the current user is the owner of the channel
+      if (userData.username === channelData.owner) {
+        toast.error("The owner cannot leave the channel");
+        return;
+      }
+
+      const success = await leaveChannel(channelId, userData.username);
+      
+      if (success) {
+        toast.success("Successfully left the channel");
+       
+        navigate(`/main`);
+      } else {
+        toast.error("Failed to leave the channel");
+      }
+    } catch (error) {
+      console.error("Error leaving channel:", error);
+      toast.error("An error occurred while trying to leave the channel");
+    }
+  }
+
   return (
     <header className="d-flex justify-content-between bg-dark text-white p-3">Header
-
+                {channelId && (
+          <button onClick={handleLeaveChannel}>Leave Channel</button>
+        )}
       <div className='general-search-bar'>
         <GeneralSearch teamId={teamId} />
       </div>
