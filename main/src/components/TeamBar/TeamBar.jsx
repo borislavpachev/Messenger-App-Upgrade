90c03cc66bd3e0bef4069bd3cf03cd2ca84a1cb9
DDankov <danko5656@gmail.com>
2024-03-13 12:37:57 +0200

    CreateTeam as modal

--- main/src/components/TeamBar/TeamBar.jsx
+++ main/src/components/TeamBar/TeamBar.jsx
@@ -1,8 +1,10 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { logoutUser } from "../../services/auth.service";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import TeamList from "../TeamList/TeamList";
+import CreateTeam from "../CreateTeam/CreateTeam"
+import './TeamBar.css';
 
 export default function TeamBar({ onItemClick }) {
   const { user, userData, setAppState } = useContext(AppContext);
@@ -20,14 +22,27 @@ export default function TeamBar({ onItemClick }) {
     navigate('/');
   }
 
+    const [showCreateTeam, setShowCreateTeam] = useState(false);
 
+    const toggleShowCreateTeam = () => {
+      setShowCreateTeam(!showCreateTeam);
+    }
 
   return (
     <div className="col-1 h-100 bg-dark text-white">
       <div className="d-stack gap-3">
         <NavLink to="/chats">Private chats</NavLink>
         <br /><br />
-        <NavLink to="/create-team">Create Team</NavLink>
+        <button className="btn-modal-create" type="button" onClick={toggleShowCreateTeam} >Create Team</button>
+        {showCreateTeam && <div className='create-team-modal'>
+          <div
+            onClick={toggleShowCreateTeam}
+            className='overlay-create-team'></div>
+          <div className='modal-create-team-content'>
+            <CreateTeam />
+            <button className='close-modal-create-btn' onClick={toggleShowCreateTeam}>Close</button>
+          </div>
+        </div>}
         <TeamList onItemClick={handleTeamClick} />
         <NavLink to="/user-profile">Profile</NavLink>
         <button onClick={logout}>Logout</button>
