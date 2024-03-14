73d68bf46120867adaee12c9c9e54cf8de7253f4
DDankov <danko5656@gmail.com>
2024-03-14 17:30:55 +0200

    auth.service offline status when log out

--- main/src/services/auth.service.js
+++ main/src/services/auth.service.js
@@ -1,5 +1,6 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../config/firebase-config';
+import { changeUserStatus } from './users.service';
 
 export const registerUser = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
@@ -9,6 +10,7 @@ export const loginUser = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
-export const logoutUser = () => {
+export const logoutUser = async (userUsername) => {
+  await changeUserStatus(userUsername, 'Offline');
   return signOut(auth);
 };
