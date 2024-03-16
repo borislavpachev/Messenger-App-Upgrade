103126b3c692adc99a042d2fb41b19f04906367e
borislavpachev <borislav.pachev@gmail.com>
2024-03-16 12:21:18 +0200

    main view css fixes

--- main/src/views/MainView/MainView.jsx
+++ main/src/views/MainView/MainView.jsx
@@ -1,15 +1,14 @@
 
-import { useState } from "react";
 import TeamBar from "../../components/TeamBar/TeamBar";
-import { Routes, Route, useParams, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import ChannelView from "../ChannelView/ChannelView";
 import Chats from "../Chats/Chats";
 import './MainView.css'
 import VideoRoom from "../../components/VideoRoom/VideoRoom";
 
 export default function MainView() {
-  const { teamId } = useParams();
-  const [selectedChat, setSelectedChat] = useState({ type: null, id: null });
+  // const { teamId } = useParams();
+  // const [selectedChat, setSelectedChat] = useState({ type: null, id: null });
   const navigate = useNavigate()
 
 
@@ -17,13 +16,13 @@ export default function MainView() {
     navigate(`/main/${teamId}`);
   }
 
-  const handleSelectChannel = (channelId) => {
-    setSelectedChat({ type: 'channel', id: channelId });
-  };
+  // const handleSelectChannel = (channelId) => {
+  //   setSelectedChat({ type: 'channel', id: channelId });
+  // };
 
-  const handleSelectPrivateChat = (chatId) => {
-    setSelectedChat({ type: 'private', id: chatId });
-  };
+  // const handleSelectPrivateChat = (chatId) => {
+  //   setSelectedChat({ type: 'private', id: chatId });
+  // };
 
   return (
     <div className="main-container">
