f01e010d13c316db45ae75fb3da046d7f2dcca2c
DDankov <danko5656@gmail.com>
2024-03-16 18:17:40 +0200

    Status Icon on status dropdown

--- main/src/components/Status/Status.jsx
+++ main/src/components/Status/Status.jsx
@@ -8,23 +8,36 @@ export default function Status() {
     const {userData} = useContext(AppContext);
 
     const usernameUser = userData.username;
+    const [status, setStatus] = useState(userData.status || 'Offline'); // Initialize status with user's current status
 
-
-    const changeStatus = async ( status) => {
-        await changeUserStatus(usernameUser, status);
+    const changeStatus = async (newStatus) => {
+        await changeUserStatus(usernameUser, newStatus);
+        setStatus(newStatus); // Update status state
     }
 
+    const statusIcon = () => {
+        switch (status) {
+            case 'Online':
+                return <BsCheckCircle color='green' size='1rem' />;
+            case 'Offline':
+                return <BsFillRecordCircleFill color='grey' size='1rem' />;
+            case 'Do not disturb':
+                return <BsFillDashCircleFill color='red' size='1rem' />;
+            default:
+                return null;
+        }
+    }
 
-  return (
-    <Dropdown className="mt-3">
-      <Dropdown.Toggle variant="secondary" id="status-dropdown">
-        Status
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-      <Dropdown.Item href="#" onClick={() => changeStatus('Online')}><BsCheckCircle  color='green' size='1rem'/>Online</Dropdown.Item>
-        <Dropdown.Item href="#" onClick={() => changeStatus('Offline')}><BsFillRecordCircleFill color='grey' size='1rem'/>Offline</Dropdown.Item>
-        <Dropdown.Item href="#" onClick={() => changeStatus('Do not disturb')}><BsFillDashCircleFill color='red' size='1rem'/>Do not disturb</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+    return (
+        <Dropdown className="mt-3">
+            <Dropdown.Toggle variant="secondary" id="status-dropdown">
+                {statusIcon()} Status {/* Display status icon */}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item href="#" onClick={() => changeStatus('Online')}><BsCheckCircle color='green' size='1rem'/>Online</Dropdown.Item>
+                <Dropdown.Item href="#" onClick={() => changeStatus('Offline')}><BsFillRecordCircleFill color='grey' size='1rem'/>Offline</Dropdown.Item>
+                <Dropdown.Item href="#" onClick={() => changeStatus('Do not disturb')}><BsFillDashCircleFill color='red' size='1rem'/>Do not disturb</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 }
