44b9ffaeb689dd13c3e47e01fd89ee90bf232ac7
borislavpachev <borislav.pachev@gmail.com>
2024-03-16 12:20:48 +0200

    useless files deleted

--- main/src/constants/agora.js
+++
@@ -1,5 +0,0 @@
-// export const agoraAppID = 'ec983962d9204bf3aff3fc8205195787';
-
-export const agoraID = 'ccd4b0df5a6547498dfd7f12d84e536b';
-export const TOKEN = '007eJxTYKi9cIXLzH9hjqE4p5Kf7+/yhGl/Yi6Y/5p1bN4eibN26jEKDMnJKSZJBilppolmpibmJpYWKWkp5mmGRikWJqmmxmZJGv8/pzYEMjLUxmQwMjJAIIjPxpCcn5OTmMTAAABvcyA3'
-export const CHANNEL = 'collab';
