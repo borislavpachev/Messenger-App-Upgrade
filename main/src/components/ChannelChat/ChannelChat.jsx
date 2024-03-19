5570e32f5a4b0495d7707935078be8cae0fc1fb7
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-03-19 18:23:05 +0200

    channel chat fixes

--- main/src/components/ChannelChat/ChannelChat.jsx
+++ main/src/components/ChannelChat/ChannelChat.jsx
@@ -15,7 +15,7 @@ import FileUpload from '../FileUpload/FileUpload';
 import EmojiPicker from '../EmojiPicker/EmojiPicker';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
-import SimpleProfilePreview from "../SimpleProfilePreview/SimpleProfilePreview";
+import SimpleProfilePreview from '../SimpleProfilePreview/SimpleProfilePreview';
 import { db } from '../../config/firebase-config';
 
 export default function ChannelChat({ channelId }) {
@@ -45,7 +45,7 @@ export default function ChannelChat({ channelId }) {
     const messagesRef = getChannelWithLiveUpdates(channelId, (newMessages) => {
       setMessages(newMessages);
     });
-  
+
     // Cleanup function
     return () => {
       off(messagesRef);
@@ -55,31 +55,30 @@ export default function ChannelChat({ channelId }) {
   const handleSend = async () => {
     if (newMessage.trim() !== '' || file) {
       try {
-        
         const fileURL = file ? await sendFile(file) : null;
-        
+
         await addChatMessage(channelId, newMessage, userData.username, fileURL);
         setNewMessage('');
         setFile(null);
-        
+        setFileName('');
         if (db) {
           const channelSnapshot = await get(ref(db, `channels/${channelId}`));
-          // rest of your code
-        
-        if (channelSnapshot.exists()) {
-          const channel = channelSnapshot.val();
-          if (channel.members) {
-            Object.values(channel.members).forEach(async member => {
-              if (member !== userData.username) {
-                await set(ref(db, `users/${member}/channels/${channelId}/isSeen`), false);                
-              }
-            });
+          if (channelSnapshot.exists()) {
+            const channel = channelSnapshot.val();
+            if (channel.members) {
+              Object.values(channel.members).forEach(async (member) => {
+                if (member !== userData.username) {
+                  await set(
+                    ref(db, `users/${member}/channels/${channelId}/isSeen`),
+                    false
+                  );
+                }
+              });
+            }
           }
-        } 
-      } else {
-        console.error('db is not defined');
-      }
-        
+        } else {
+          console.error('db is not defined');
+        }
       } catch (error) {
         console.error('Failed to send message:', error);
       }
@@ -104,16 +103,20 @@ export default function ChannelChat({ channelId }) {
     setEditMessageContent(messageToEdit.message);
   };
 
-  const handleUpdate = async () => {
-    if (editMessageContent.trim() !== '' || file) {
-      const messageToEdit = messages.find(
-        (message) => message.id === editMessageId
-      );
-      if (messageToEdit.sender !== userData.username) {
-        return;
-      }
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    const messageToEdit = messages.find(
+      (message) => message.id === editMessageId
+    );
+    if (messageToEdit.sender !== userData.username) {
+      return;
+    }
+    if (editMessageContent.trim() !== '' || file || messageToEdit.fileURL) {
       try {
         const fileURL = file ? await sendFile(file) : messageToEdit.fileURL;
+        if (file && fileURL === undefined) {
+          throw new Error('File upload failed');
+        }
         await editChatMessage(
           channelId,
           editMessageId,
@@ -130,6 +133,7 @@ export default function ChannelChat({ channelId }) {
         setEditMessageId(null);
         setEditMessageContent('');
         setFile(null);
+        setFileName('');
       } catch (error) {
         console.log(error.message);
       }
@@ -219,76 +223,85 @@ export default function ChannelChat({ channelId }) {
     setShowEmojis(!showEmojis);
   };
 
-
   return (
     <div className="chan-chat-container">
-        <div className="messages-container">
+      <div className="messages-container">
         {messages &&
-                messages.map((message, index) => (
-                    <div key={message.id} ref={index === messages.length - 1 ? lastMessageRef : null} className={message.sender === userData.username ? "my-message" : "edit-chat-message"}>
-                        <SimpleProfilePreview username={message.sender} date={new Date(message.sentOn).toLocaleString()} />
-                        <p>
-                            {makeLinkMessage(message.message)}
-                        </p>
-                        {message.fileURL && (
-                            <img
-                                src={message.fileURL}
-                                alt="Message attachment"
-                                className="uploaded-message-img"
-                            />
-                        )}
-                        {message.sender === userData.username && (
-                            <>
-                                <button onClick={() => handleEditButtonClick(message.id)}>
-                                    Edit
-                                </button>
-                                <button onClick={() => handleDelete(message.id)}>
-                                    Delete
-                                </button>
-                            </>
-                        )}
-                    </div>
-                ))}
-        </div>
-        <form onSubmit={(e) => e.preventDefault()} encType="multipart/form-data">
-            <div className="chat-input-field">
-                <FileUpload
-                    file={file}
-                    fileName={fileName}
-                    fileChange={handleFileChange}
-                    removeFile={removeFile}
-                />
-                <textarea
-                    ref={inputRef}
-                    type="text"
-                    name="message"
-                    id="message"
-                    value={editMessageId ? editMessageContent : newMessage}
-                    onChange={
-                        editMessageId
-                            ? (e) => setEditMessageContent(e.target.value)
-                            : (e) => setNewMessage(e.target.value)
-                    }
-                    className="chat-message-input"
-                />
-                <FontAwesomeIcon
-                    className="emoji-button"
-                    onClick={handleShowEmojis}
-                    icon={faFaceSmile}
-                />
-                {showEmojis && (
-                    <div className="channel-chat-emoji-picker">
-                        <EmojiPicker onEmojiSelect={handleEmojiSelect} />
-                    </div>
-                )}
-            </div>
-            <button
-                type="submit"
-                onClick={editMessageId ? handleUpdate : handleSend}
+          messages.map((message, index) => (
+            <div
+              key={message.id}
+              ref={index === messages.length - 1 ? lastMessageRef : null}
+              className={
+                message.sender === userData.username
+                  ? 'my-message'
+                  : 'edit-chat-message'
+              }
             >
-                {editMessageId ? 'Update' : 'Send'}
-            </button>
-        </form>
+              <SimpleProfilePreview
+                username={message.sender}
+                date={new Date(message.sentOn).toLocaleString()}
+              />
+              <p>{makeLinkMessage(message.message)}</p>
+              {message.fileURL && (
+                <img
+                  src={message.fileURL}
+                  alt="Message attachment"
+                  className="uploaded-message-img"
+                />
+              )}
+              {message.sender === userData.username && (
+                <>
+                  <button onClick={() => handleEditButtonClick(message.id)}>
+                    Edit
+                  </button>
+                  <button onClick={() => handleDelete(message.id)}>
+                    Delete
+                  </button>
+                </>
+              )}
+            </div>
+          ))}
+      </div>
+      <form onSubmit={(e) => e.preventDefault()} encType="multipart/form-data">
+        <div className="chat-input-field">
+          <FileUpload
+            file={file}
+            fileName={fileName}
+            fileChange={handleFileChange}
+            removeFile={removeFile}
+          />
+          <textarea
+            ref={inputRef}
+            type="text"
+            name="message"
+            id="message"
+            value={editMessageId ? editMessageContent : newMessage}
+            onChange={
+              editMessageId
+                ? (e) => setEditMessageContent(e.target.value)
+                : (e) => setNewMessage(e.target.value)
+            }
+            className="chat-message-input"
+          />
+          <FontAwesomeIcon
+            className="emoji-button"
+            onClick={handleShowEmojis}
+            icon={faFaceSmile}
+          />
+          {showEmojis && (
+            <div className="channel-chat-emoji-picker">
+              <EmojiPicker onEmojiSelect={handleEmojiSelect} />
+            </div>
+          )}
+          <button
+            type="submit"
+            onClick={editMessageId ? handleUpdate : handleSend}
+            className="send-message"
+          >
+            {editMessageId ? 'Update' : 'Send'}
+          </button>
+        </div>
+      </form>
     </div>
-);
+  );
 }
