01652b5ddf3cc102b503d22ffeebae89b8eccc8a
borislavpachev <borislav.pachev@gmail.com>
2024-05-13 17:54:53 +0300

    create team modal fix

--- main/src/components/CreateTeam/CreateTeam.jsx
+++ main/src/components/CreateTeam/CreateTeam.jsx
@@ -3,10 +3,12 @@ import Button from '../../components/Button/Button';
 import { AppContext } from '../../context/AppContext';
 import { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { checkTeamNameExists, createNewTeam } from '../../services/teams.service';
+import {
+  checkTeamNameExists,
+  createNewTeam,
+} from '../../services/teams.service';
 import { createChannel } from '../../services/channel.service';
 import { getAllUsers } from '../../services/users.service';
-import './CreateTeam.css'
 
 export default function CreateTeam({ toggleShowCreateTeam }) {
   const navigate = useNavigate();
@@ -21,7 +23,7 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
 
   useEffect(() => {
     if (userData) {
-      setForm(prevForm => ({
+      setForm((prevForm) => ({
         ...prevForm,
         teamOwner: userData.username,
         teamMembers: [userData.username],
@@ -30,14 +32,11 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
   }, [userData]);
 
   const teamNameHandler = (event) => {
-    setForm(prevForm => ({ ...prevForm, teamName: event.target.value }));
+    setForm((prevForm) => ({ ...prevForm, teamName: event.target.value }));
   };
 
   const createTeam = async () => {
-    if (
-      form.teamName.length < 3 ||
-      form.teamName.length > 40
-    ) {
+    if (form.teamName.length < 3 || form.teamName.length > 40) {
       toast.error('Name must be between 3 and 40 symbols.');
       return;
     }
@@ -50,14 +49,26 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
     }
 
     try {
-      const newTeamRef = await createNewTeam(form.teamName, form.teamOwner, form.teamMembers, form.teamChannels);
+      const newTeamRef = await createNewTeam(
+        form.teamName,
+        form.teamOwner,
+        form.teamMembers,
+        form.teamChannels
+      );
       const teamId = newTeamRef.key;
       const initialChat = {
         text: `Welcome to the General channel!`,
         sender: form.teamOwner,
         timeStamp: Date.now(),
       };
-      await createChannel(teamId, form.teamOwner, 'General', initialChat, form.teamMembers, false);
+      await createChannel(
+        teamId,
+        form.teamOwner,
+        'General',
+        initialChat,
+        form.teamMembers,
+        false
+      );
       toast.success(`Team ${form.teamName} created successfully.`);
       toggleShowCreateTeam();
     } catch (error) {
@@ -65,31 +76,31 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
     }
   };
 
-    const handleAddUser = (username) => {
-      if (username && username.trim() !== "") {
-        setForm(prevForm => ({
-          ...prevForm,
-          teamMembers: [...prevForm.teamMembers, username]
-        }));
-        toast.success(`User ${username} added to team`);
-      }
-    };
-
-
-    const [searchInput, setSearchInput] = useState('');
-
-    const [searchResults, setSearchResults] = useState([]);
-
-    const [searchPerformed, setSearchPerformed] = useState(false);
-
-  
-    const updateFormSearch = e => {
-      setSearchInput(e.target.value)
+  const handleAddUser = (username) => {
+    if (username && username.trim() !== '') {
+      setForm((prevForm) => ({
+        ...prevForm,
+        teamMembers: [...prevForm.teamMembers, username],
+      }));
+      toast.success(`User ${username} added to team`);
     }
+  };
+
+  const [searchInput, setSearchInput] = useState('');
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  const [searchPerformed, setSearchPerformed] = useState(false);
+
+  const updateFormSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
 
   const searchUsers = async () => {
     const allUsers = await getAllUsers();
-    const filteredUsers = allUsers.filter(user => user.username.startsWith(searchInput));
+    const filteredUsers = allUsers.filter((user) =>
+      user.username.startsWith(searchInput)
+    );
     return filteredUsers;
   };
 
@@ -98,7 +109,7 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
     const results = await searchUsers();
     setSearchResults(results);
     setSearchPerformed(true);
-  }
+  };
 
   useEffect(() => {
     if (searchInput === '') {
@@ -108,35 +119,57 @@ export default function CreateTeam({ toggleShowCreateTeam }) {
 
   return (
     <>
-      <div className="create-team">
-        <h3 className="create-team-head">Make you own Team</h3>
-        <div className="team-name">
-          <form>
-            <div className="team-name-input">
-              <input placeholder='Your new team name' className='create-team-name-input' onChange={teamNameHandler} type="text" />
-            </div>
-            <button title="create-team-btn" className='create-team-button' onClick={(event) => { event.preventDefault(); createTeam(); }}>Create Team</button>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex align-items-center justify-content-center mt-2">
+            <span className="text-black me-1">Add team members:</span>
+            <input
+              autoComplete="off"
+              className="form-control w-50"
+              type="search"
+              placeholder="Search by username"
+              value={searchInput}
+              onChange={updateFormSearch}
+            />
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Search
+            </button>
+          </div>
+          <div className="border border-dark m-4">
+            {searchResults.map((user, index) => (
+              <div className="d-inline-flex m-3" key={index}>
+                <div className="d-flex justify-content-center align-items-center">
+                  <p className="fs-6 m-1">{user.username}</p>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleAddUser(user.username)}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </form>
+        <div>
+          <form className="w-100">
+            <input
+              placeholder="Your Team Name"
+              className="form-control"
+              onChange={teamNameHandler}
+              type="text"
+            />
+            <button
+              className="btn btn-primary m-2 w-100 p-2"
+              onClick={(event) => {
+                event.preventDefault();
+                createTeam();
+              }}
+            >
+              Create Team
+            </button>
           </form>
         </div>
-        <form className="add-team-memberes-create-form" onSubmit={handleSubmit}>
-        <h3 className="search-users-create-head">Add users</h3>
-        <input autoComplete="off" className="form-control-create-team"
-          type="text" placeholder='Search by username'
-          value={searchInput} onChange={updateFormSearch} />
-        <button className="search-button-create-team" onClick={handleSubmit}>Search</button>
-        <div className="search-results-create-team">
-          {searchResults.map((user, index) => (
-            <div className="search-results-item-create-team" key={index}>
-              <div className="user-info-create-team">
-                {user.username}
-              </div>
-              <div className="use-actions-create-team">
-                <button className='add-button-new-team' onClick={() => handleAddUser(user.username)}>Add</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </form>
       </div>
     </>
   );
