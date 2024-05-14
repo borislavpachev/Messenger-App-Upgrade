cb7ffa84060a2daa25ce76f3b7ea09fe1e7bac5b
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 10:55:00 +0300

    app loading problem fixed

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,23 +1,22 @@
 import { useEffect, useState } from 'react';
-import { AppContext } from './context/AppContext'
+import { AppContext } from './context/AppContext';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './config/firebase-config';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { changeUserStatus, getUserData } from './services/users.service'
+import { changeUserStatus, getUserData } from './services/users.service';
 import Authenticated from './hoc/Authenticated';
-import Login from './views/Login/Login'
-import ForgotPassword from './views/ForgotPassword/ForgotPassword'
-import CreateAccount from './views/CreateAccount/CreateAccount'
-import UpdateAccount from './views/UpdateAccount/UpdateAccount'
-import MainView from './views/MainView/MainView'
-import ErrorPage from './views/ErrorPage/ErrorPage'
+import Login from './views/Login/Login';
+import ForgotPassword from './views/ForgotPassword/ForgotPassword';
+import CreateAccount from './views/CreateAccount/CreateAccount';
+import UpdateAccount from './views/UpdateAccount/UpdateAccount';
+import MainView from './views/MainView/MainView';
+import ErrorPage from './views/ErrorPage/ErrorPage';
 import { Toaster } from 'react-hot-toast';
-import Loader from './components/Loader/Loader'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import CallNotification from './components/CallNotification/CallNotification'
+import Loader from './components/Loader/Loader';
+import CallNotification from './components/CallNotification/CallNotification';
 import { IsSeenProvider } from './context/IsSeenProvider';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   const [appState, setAppState] = useState({
@@ -31,57 +30,70 @@ function App() {
   });
   const [user, loading, error] = useAuthState(auth);
 
-
   useEffect(() => {
     if (user) {
-      setAppState(prevState => ({ ...prevState, isLoading: true }));
+      setAppState((prevState) => ({ ...prevState, isLoading: true }));
 
-      getUserData(user.uid)
-        .then(snapshot => {
-          if (snapshot.exists()) {
-            setAppState({ user, userData: snapshot.val()[Object.keys(snapshot.val())[0]] });
-            const userData = snapshot.val();
-            const username = userData[Object.keys(userData)[0]].username;
-            changeUserStatus(username, 'Online')
+      getUserData(user.uid).then((snapshot) => {
+        if (snapshot.exists()) {
+          setAppState({
+            user,
+            userData: snapshot.val()[Object.keys(snapshot.val())[0]],
+          });
+          const userData = snapshot.val();
+          const username = userData[Object.keys(userData)[0]].username;
+          changeUserStatus(username, 'Online');
 
-
-            setAppState(prevState => ({
-              ...prevState,
-              user,
-              userData: userData[Object.keys(userData)[0]],
-              isLoading: false,
-            }));
-          }
-        })
+          setAppState((prevState) => ({
+            ...prevState,
+            user,
+            userData: userData[Object.keys(userData)[0]],
+            isLoading: false,
+          }));
+        }
+      });
     }
   }, [user]);
 
-  if (loading) {
-    return <Loader />
+  if (loading || appState.isLoading) {
+    return <Loader />;
   }
 
   return (
     <>
-   
       <BrowserRouter>
         <AppContext.Provider value={{ ...appState, setAppState }}>
-        <CallNotification />
+          <CallNotification />
           <Toaster />
           <IsSeenProvider>
-          <Routes>
-            <Route index element={<Login />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/create-account' element={<CreateAccount />} />
-            <Route path='/forgot-password' element={<ForgotPassword />} />
-            <Route path='/update-profile' element={<Authenticated><UpdateAccount /></Authenticated>} />
-            <Route path='/main/*' element={<Authenticated><MainView /></Authenticated>} />
-            <Route path='*' element={<ErrorPage/>} />
-          </Routes>
+            <Routes>
+              <Route index element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/create-account" element={<CreateAccount />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route
+                path="/update-profile"
+                element={
+                  <Authenticated>
+                    <UpdateAccount />
+                  </Authenticated>
+                }
+              />
+              <Route
+                path="/main/*"
+                element={
+                  <Authenticated>
+                    <MainView />
+                  </Authenticated>
+                }
+              />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
           </IsSeenProvider>
         </AppContext.Provider>
-      </BrowserRouter >
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
