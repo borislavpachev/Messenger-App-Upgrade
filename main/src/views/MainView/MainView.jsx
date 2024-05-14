ae08f8ed89c3a491a7e8ab4f366b1c074aefc3ad
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 10:56:10 +0300

    main view css fixes

--- main/src/views/MainView/MainView.jsx
+++ main/src/views/MainView/MainView.jsx
@@ -1,58 +1,51 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import ChannelView from "../ChannelView/ChannelView";
-import Chats from "../Chats/Chats";
-import './MainView.css'
-import VideoRoom from "../../components/VideoRoom/VideoRoom";
-import Header from "../../components/Header/Header";
-import { getChannelIdByTitle } from "../../services/channel.service";
-import { useContext } from "react";
-import { AppContext } from "../../context/AppContext";
-import TeamBarComponent from "../../components/TeamBar/TeamBarComponent/TeamBarComponent";
+import ChannelView from '../ChannelView/ChannelView';
+import Chats from '../Chats/Chats';
+import VideoRoom from '../../components/VideoRoom/VideoRoom';
+import Header from '../../components/Header/Header';
+import { getChannelIdByTitle } from '../../services/channel.service';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+import TeamBarComponent from '../../components/TeamBar/TeamBarComponent/TeamBarComponent';
 import WelcomeView from '../WelcomeView/WelcomeView';
-
+import './MainView.css';
 
 export default function MainView() {
-  const { userData } = useContext(AppContext)
-  // const { teamId } = useParams();
-  // const [selectedChat, setSelectedChat] = useState({ type: null, id: null });
-  const navigate = useNavigate()
-
+  const { userData } = useContext(AppContext);
+  const navigate = useNavigate();
 
   const handleSelectTeam = async (teamId) => {
-    // Fetch the ID of the specific channel for the team
-    const channelId = await getChannelIdByTitle(teamId, 'General', userData.username);
-  
-    // Navigate to the specific channel for the team
+    const channelId = await getChannelIdByTitle(
+      teamId,
+      'General',
+      userData.username
+    );
+
     navigate(`${teamId}/channels/${channelId}`);
   };
 
-  // const handleSelectChannel = (channelId) => {
-  //   setSelectedChat({ type: 'channel', id: channelId });
-  // };
-
-  // const handleSelectPrivateChat = (chatId) => {
-  //   setSelectedChat({ type: 'private', id: chatId });
-  // };
-
   return (
-    <div className="main-container">
-              {/* <div className='main-test'> */}
-      <div className="main-view-bar">
-        <TeamBarComponent onItemClick={handleSelectTeam} />
+    <div className='main-view-custom-height'>
+      <div className='header-height'>
+        <Header onItemClick={handleSelectTeam} />
       </div>
-      <div className="main-view-content">
-              <Header onItemClick={handleSelectTeam}/>
-        <Routes>
-        <Route index element={<WelcomeView />} />
-          <Route path="/chats" element={<Chats />} />
-          <Route path="/chats/:id" element={<Chats />} />
-          <Route path='/chats/video' element={<VideoRoom />} />
-          <Route path='/chats/video/:chatId' element={<VideoRoom />} />
-          <Route path="/:teamId" element={<ChannelView/>}>
-            <Route index element={<div />} />
-            <Route path="channels/:channelId" element={<ChannelView />} />
-          </Route>
-        </Routes>
+      <div className="row content-height">
+        <div className="col-1">
+          <TeamBarComponent onItemClick={handleSelectTeam} />
+        </div>
+        <div className="col-11">
+          <Routes>
+            <Route index element={<WelcomeView />} />
+            <Route path="/chats" element={<Chats />} />
+            <Route path="/chats/:id" element={<Chats />} />
+            <Route path="/chats/video" element={<VideoRoom />} />
+            <Route path="/chats/video/:chatId" element={<VideoRoom />} />
+            <Route path="/:teamId" element={<ChannelView />}>
+              <Route index element={<div />} />
+              <Route path="channels/:channelId" element={<ChannelView />} />
+            </Route>
+          </Routes>
+        </div>
       </div>
     </div>
   );
