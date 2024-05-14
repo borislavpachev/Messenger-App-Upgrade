4ff62e345a5603c22d9cacc655569acab4d619f6
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 10:43:56 +0300

    chats view css fixes

--- main/src/views/Chats/Chats.jsx
+++ main/src/views/Chats/Chats.jsx
@@ -1,37 +1,39 @@
 import { useContext, useEffect, useState } from 'react';
 import ChatContent from '../../components/ChatComponents/ChatContent/ChatContent';
-import CreateChatRoom from "../../components/ChatComponents/CreateChatRoom/CreateChatRoom";
-import UserChats from "../../components/ChatComponents/UserChats/UserChats";
+import CreateChatRoom from '../../components/ChatComponents/CreateChatRoom/CreateChatRoom';
+import UserChats from '../../components/ChatComponents/UserChats/UserChats';
 import { getChatByParticipant } from '../../services/chats.service';
 import { AppContext } from '../../context/AppContext';
-import { useNavigate, useParams } from 'react-router-dom';
-import './Chats.css'
+import { useParams } from 'react-router-dom';
+import './Chats.css';
 
 export default function Chats() {
-    const { userData } = useContext(AppContext);
-    const [chats, setChats] = useState([]);
-    const { id } = useParams();
+  const { userData } = useContext(AppContext);
+  const [chats, setChats] = useState([]);
+  const { id } = useParams();
 
+  useEffect(() => {
+    const unsubscribe = getChatByParticipant(userData.username, setChats);
 
-    useEffect(() => {
-        const unsubscribe = getChatByParticipant(userData.username, setChats);
+    return () => unsubscribe;
+  }, [userData.username]);
 
-        return () => unsubscribe;
-    }, [userData.username]);
+  const sortedChats = [...chats].sort((a, b) => {
+    return new Date(b.lastModified) - new Date(a.lastModified);
+  });
 
-    const sortedChats = [...chats].sort((a, b) => {
-        return new Date(b.lastModified) - new Date(a.lastModified);
-    });
-
-    return (
-        <div className='chat-main'>
-            <div className='chat-side'>
-                <CreateChatRoom />
-                <UserChats chats={sortedChats} />
-            </div>
-            <div className='chat-content'>
-                <ChatContent chatId={id} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="d-flex chat-main-custom-margin">
+      <div
+        className="user-chats-custom-height
+      d-flex flex-column w-25 border-end border-light"
+      >
+        <CreateChatRoom />
+        <UserChats chats={sortedChats} />
+      </div>
+      <div className="w-75 user-chats-custom-height">
+        <ChatContent chatId={id} />
+      </div>
+    </div>
+  );
 }
