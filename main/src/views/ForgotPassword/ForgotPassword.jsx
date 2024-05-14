67985a6ad87c0968427e5df916e2e14d42278977
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 10:55:40 +0300

    css fixes

--- main/src/views/ForgotPassword/ForgotPassword.jsx
+++ main/src/views/ForgotPassword/ForgotPassword.jsx
@@ -1,49 +1,63 @@
-import { useState } from "react"
-import { sendPasswordResetEmail } from "firebase/auth"
-import { auth } from "../../config/firebase-config";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
-import './ForgotPassword.css';
-
+import { useState } from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../config/firebase-config';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 export default function ForgotPassword() {
-    const [email, setEmail] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
 
-    const handleSubmit = async () => {
-
-        try {
-            if (email) {
-                await sendPasswordResetEmail(auth, email)
-                toast.success('A reset link has been send to your email');
-                navigate('/');
-            } else {
-                toast.error('The email field cannot be empty!')
-            }
-        } catch (error) {
-            if (error.message.includes('not-found')) {
-                toast.error('This email does not exist');
-            } else {
-                toast.error(error.code);
-            }
-
-        }
+  const handleSubmit = async () => {
+    try {
+      if (email) {
+        await sendPasswordResetEmail(auth, email);
+        toast.success('A reset link has been send to your email');
+        navigate('/');
+      } else {
+        toast.error('The email field cannot be empty!');
+      }
+    } catch (error) {
+      if (error.message.includes('not-found')) {
+        toast.error('This email does not exist');
+      } else {
+        toast.error(error.code);
+      }
     }
+  };
 
-    return (
-        <div className="forgot-wrapper">
-            <div className="forgot-psw">
-                <h2 className="login-header">Forgot Password</h2>
-                <form name='reset-password' onSubmit={e => e.preventDefault()}>
-                    <div className="form-group mb-2 ">
-                        <label className="form-label" htmlFor="email">Email: </label>
-                        <input className="form-control" type="email" id="email" name="email"
-                            value={email} onChange={(e) => setEmail(e.target.value)} />
+  return (
+    <div
+      className="container justify-content-center align-items-center 
+        rounded bg-dark text-white p-5 mt-5 w-25"
+    >
+      <h2 className="text-center">Forgot Password</h2>
+      <form
+        name="reset-password"
+        className="mt-3"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <div className="form-group d-flex flex-column">
+          <label htmlFor="email2" className='mb-2 form-label'>Email Address: </label>
+          <input
+            className="form-control"
+            type="email"
+            id="email2"
+            name="email2"
+            value={email}   
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail"
 
-                        <button className="login-button" onClick={handleSubmit} type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
+          />
+          <button
+            className="btn btn-primary px-5 py-3 m-3 align-self-center"
+            onClick={handleSubmit}
+            type="submit"
+          >
+            Submit
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
--- main/src/views/UpdateAccount/UpdateAccount.jsx
+++ main/src/views/UpdateAccount/UpdateAccount.jsx
@@ -1,127 +1,218 @@
-import { useContext, useState, useEffect } from "react";
-import { AppContext } from "../../context/AppContext";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import './UpdateAccount.css'
-import { updateUser } from "../../services/users.service";
-import { updateEmail, updatePassword } from 'firebase/auth'
-import { logoutUser } from "../../services/auth.service";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-
+import './UpdateAccount.css';
+import { updateUser } from '../../services/users.service';
+import { updateEmail, updatePassword } from 'firebase/auth';
+import { logoutUser } from '../../services/auth.service';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export default function UpdateAccount() {
-    const { user, userData, setAppState } = useContext(AppContext);
-    const [showPassword, setShowPassword] = useState({
-        password: false,
-        confirmPassword: false
-    });
-    const [form, setForm] = useState({
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        email: userData.email,
-        phoneNumber: userData.phoneNumber,
-        password: '',
-        confirmPassword: '',
-    });
-    const navigate = useNavigate();
+  const { user, userData, setAppState } = useContext(AppContext);
+  const [showPassword, setShowPassword] = useState({
+    password: false,
+    confirmPassword: false,
+  });
+  const [form, setForm] = useState({
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    email: userData.email,
+    phoneNumber: userData.phoneNumber,
+    password: '',
+    confirmPassword: '',
+  });
+  const navigate = useNavigate();
 
-    const updateForm = prop => e => {
-        setForm({ ...form, [prop]: e.target.value })
-    };
+  const updateForm = (prop) => (e) => {
+    setForm({ ...form, [prop]: e.target.value });
+  };
 
-    const updateUserProfile = async () => {
-
-        try {
-            if (form.confirmPassword.length && form.password === form.confirmPassword) {
-                await updatePassword(user, form.confirmPassword);
-                const interval = setInterval(() => {
-                    toast.success('Password has been changed successfully!');
-                }, 200);
-                setTimeout(() => { clearInterval(interval) }, 400);
-                await logoutUser();
-            }
-            form.email !== userData.email ? await updateEmail(user, form.email) : null
-            await updateUser(userData.username, form.firstName, form.lastName, form.email, userData.uid, form.phoneNumber);
-            setAppState({ user: null, userData: null });
-            await logoutUser();
-            const interval = setInterval(() => {
-                toast.success('Account updated successfully!');
-            }, 100);
-            setTimeout(() => { clearInterval(interval) }, 300);
-            navigate('/');
-        } catch (error) {
-            toast.error(error.message);
-        }
+  const updateUserProfile = async () => {
+    try {
+      if (
+        form.confirmPassword.length &&
+        form.password === form.confirmPassword
+      ) {
+        await updatePassword(user, form.confirmPassword);
+        const interval = setInterval(() => {
+          toast.success('Password has been changed successfully!');
+        }, 200);
+        setTimeout(() => {
+          clearInterval(interval);
+        }, 400);
+        await logoutUser();
+      }
+      form.email !== userData.email
+        ? await updateEmail(user, form.email)
+        : null;
+      await updateUser(
+        userData.username,
+        form.firstName,
+        form.lastName,
+        form.email,
+        userData.uid,
+        form.phoneNumber
+      );
+      setAppState({ user: null, userData: null });
+      await logoutUser();
+      const interval = setInterval(() => {
+        toast.success('Account updated successfully!');
+      }, 100);
+      setTimeout(() => {
+        clearInterval(interval);
+      }, 300);
+      navigate('/');
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    return (
-        <div>
-            <div className="background-wrapper">
-                <div className="update-account">
-                    <h2 className="heading mb-3 text-center">Update Account</h2>
-                    <form onSubmit={e => e.preventDefault()} >
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="username">Username: </label>
-                            <input readOnly disabled autoComplete="off" className="form-control"
-                                type="text" name="username" id="username"
-                                value={userData.username} />
-                        </div>
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="email">Your e-mail: </label>
-                            <input autoComplete="off" className="form-control"
-                                type="email" name="email" id="email" value={form.email}
-                                onChange={updateForm('email')} />
-                        </div>
+  return (
+    <div>
+      <div
+        className="container align-items-center justify-content-center
+      rounded bg-dark text-white p-4 mt-3 w-25"
+      >
+        <h2 className="mb-3 text-center">Update Account</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="username">
+              Username:{' '}
+            </label>
+            <input
+              readOnly
+              disabled
+              autoComplete="off"
+              className="form-control"
+              type="text"
+              name="username"
+              id="username"
+              value={userData.username}
+            />
+          </div>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="email">
+              Your e-mail:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type="email"
+              name="email"
+              id="email"
+              value={form.email}
+              onChange={updateForm('email')}
+            />
+          </div>
 
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="first-name">First Name: </label>
-                            <input autoComplete="off" className="form-control" type="text"
-                                name="first-name" id="first-name" value={form.firstName}
-                                onChange={updateForm('firstName')} />
-                        </div>
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="last-name">Last Name: </label>
-                            <input autoComplete="off" className="form-control"
-                                type="text" name="last-name" id="last-name"
-                                value={form.lastName} onChange={updateForm('lastName')} />
-                        </div>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="first-name">
+              First Name:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type="text"
+              name="first-name"
+              id="first-name"
+              value={form.firstName}
+              onChange={updateForm('firstName')}
+            />
+          </div>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="last-name">
+              Last Name:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type="text"
+              name="last-name"
+              id="last-name"
+              value={form.lastName}
+              onChange={updateForm('lastName')}
+            />
+          </div>
 
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="phone-number">Phone Number: </label>
-                            <input autoComplete="off" className="form-control"
-                                type="text" name="phone" id="phone-number"
-                                value={form.phoneNumber} onChange={updateForm('phoneNumber')} />
-                        </div>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="phone-number">
+              Phone Number:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type="text"
+              name="phone"
+              id="phone-number"
+              value={form.phoneNumber}
+              onChange={updateForm('phoneNumber')}
+            />
+          </div>
 
-                        <hr className="hr-line" />
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="password">New password: </label>
-                            <input autoComplete="off"
-                                className="form-control" type={showPassword.password ? 'text' : 'password'}
-                                name="password" id="password" value={form.password}
-                                onChange={updateForm('password')} />
-                            <span className="password-span"><FontAwesomeIcon
-                                onClick={() => setShowPassword({ ...showPassword, password: !showPassword.password })}
-                                icon={showPassword.password ? faEye : faEyeSlash} />
-                            </span>
-                        </div>
-                        <div className="form-group mb-2 ">
-                            <label className="form-label" htmlFor="confirm-password">Confirm password: </label>
-                            <input autoComplete="off"
-                                className="form-control" type={showPassword.confirmPassword ? 'text' : 'password'}
-                                name="confirm-password" id="confirm-password" value={form.confirmPassword}
-                                onChange={updateForm('confirmPassword')} />
-                            <span className="password-span"><FontAwesomeIcon
-                                onClick={() => setShowPassword({ ...showPassword, confirmPassword: !showPassword.confirmPassword })}
-                                icon={showPassword.confirmPassword ? faEye : faEyeSlash} />
-                            </span>
-                        </div>
-                        <button type="submit" className="login-button"
-                            onClick={updateUserProfile}>Update account</button>
-                    </form>
-                </div>
-            </div >
-        </div>
-    )
+          <hr className="hr-line" />
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="password">
+              New password:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type={showPassword.password ? 'text' : 'password'}
+              name="password"
+              id="password"
+              value={form.password}
+              onChange={updateForm('password')}
+            />
+            <span className="password-span">
+              <FontAwesomeIcon
+                onClick={() =>
+                  setShowPassword({
+                    ...showPassword,
+                    password: !showPassword.password,
+                  })
+                }
+                icon={showPassword.password ? faEye : faEyeSlash}
+              />
+            </span>
+          </div>
+          <div className="form-group mb-2 ">
+            <label className="form-label" htmlFor="confirm-password">
+              Confirm password:{' '}
+            </label>
+            <input
+              autoComplete="off"
+              className="form-control"
+              type={showPassword.confirmPassword ? 'text' : 'password'}
+              name="confirm-password"
+              id="confirm-password"
+              value={form.confirmPassword}
+              onChange={updateForm('confirmPassword')}
+            />
+            <span className="password-span">
+              <FontAwesomeIcon
+                onClick={() =>
+                  setShowPassword({
+                    ...showPassword,
+                    confirmPassword: !showPassword.confirmPassword,
+                  })
+                }
+                icon={showPassword.confirmPassword ? faEye : faEyeSlash}
+              />
+            </span>
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <button
+              type="submit"
+              className="btn btn-primary px-5 py-3 mt-2 justify-self-center"
+              onClick={updateUserProfile}
+            >
+              Update account
+            </button>
+          </div>
+        </form>{' '}
+      </div>
+    </div>
+  );
 }
