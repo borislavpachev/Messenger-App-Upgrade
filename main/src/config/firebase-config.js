7a7746102159bd37de3848b79684d0bd04a0890a
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 13:37:31 +0300

    syntax fixes

--- main/src/config/firebase-config.js
+++ main/src/config/firebase-config.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyA6ZcjXjo7RQt9d44WJCrVJtxGFjwuMk78",
     authDomain: "collab-app-79dd7.firebaseapp.com",
@@ -18,11 +13,8 @@ const firebaseConfig = {
     databaseURL: "https://collab-app-79dd7-default-rtdb.europe-west1.firebasedatabase.app/"
   };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-// the Realtime Database handler
 export const db = getDatabase(app);
-
 export const storage = getStorage(app);
