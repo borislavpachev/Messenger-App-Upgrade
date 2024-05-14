f19d7f24db64dce56bd8d6ab38b6279ac9af9b0b
borislavpachev <borislav.pachev@gmail.com>
2024-05-14 10:56:51 +0300

    css fixes

--- main/src/views/CreateChannel/CreateChannel.jsx
+++ main/src/views/CreateChannel/CreateChannel.jsx
@@ -1,84 +1,125 @@
-import { useContext, useState, useEffect } from "react";
-import { createChannel } from "../../services/channel.service";
-import { AppContext } from "../../context/AppContext";
-import {  getTeamMembersByTeamId } from '../../services/teams.service';
+import { useContext, useState, useEffect } from 'react';
+import { createChannel } from '../../services/channel.service';
+import { AppContext } from '../../context/AppContext';
+import { getTeamMembersByTeamId } from '../../services/teams.service';
 import PropTypes from 'prop-types';
 import ReactSelect, { components } from 'react-select';
 
-export default function CreateChannel( {teamId, handleClose, onChannelCreated} ) {
-    const { userData } = useContext(AppContext)
-    const [title, setTitle] = useState('');
-    const [members, setMembers] = useState([]);
-    const [chat, setChat] = useState({});
-    const [teamMembers, setTeamMembers] = useState([]);
-    const [isPrivate, setIsPrivate] = useState(false);
+export default function CreateChannel({
+  teamId,
+  handleClose,
+  onChannelCreated,
+}) {
+  const { userData } = useContext(AppContext);
+  const [title, setTitle] = useState('');
+  const [members, setMembers] = useState([]);
+  const [chat, setChat] = useState({});
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [isPrivate, setIsPrivate] = useState(false);
 
-    useEffect(() => {
-      getTeamMembersByTeamId(teamId)
-        .then(members => {
-          const otherMembers = members.filter(memberName => memberName !== userData.username);
-          setTeamMembers(otherMembers);
-        })
-        .catch(error => {
-          console.error("Error fetching team members: ", error);
-        });
-    }, [teamId, userData.username]);
-
-    const handleSubmitChannel = async (event) => {
-        event.preventDefault();        
-
-        try {
-          const allMembers = [...members, userData.username];
-          await createChannel(teamId, userData.username, title, chat, allMembers, isPrivate)
-            setTitle('');
-            setMembers([]);
-            setChat({});
-            setIsPrivate(false);
-            onChannelCreated();
-            handleClose();
-        } catch (error){
-            console.error('Failed to create channel:', error)
-        }
-    }
-
-    const options = teamMembers.map(memberName => ({ value: memberName, label: memberName }));
-
-    const Option = props => {
-        return (
-          <div>
-            <components.Option {...props}>
-              {props.isSelected ? '✔' : ''} {props.label}
-            </components.Option>
-          </div>
+  useEffect(() => {
+    getTeamMembersByTeamId(teamId)
+      .then((members) => {
+        const otherMembers = members.filter(
+          (memberName) => memberName !== userData.username
         );
-      };
+        setTeamMembers(otherMembers);
+      })
+      .catch((error) => {
+        console.error('Error fetching team members: ', error);
+      });
+  }, [teamId, userData.username]);
 
+  const handleSubmitChannel = async (event) => {
+    event.preventDefault();
+
+    try {
+      const allMembers = [...members, userData.username];
+      await createChannel(
+        teamId,
+        userData.username,
+        title,
+        chat,
+        allMembers,
+        isPrivate
+      );
+      setTitle('');
+      setMembers([]);
+      setChat({});
+      setIsPrivate(false);
+      onChannelCreated();
+      handleClose();
+    } catch (error) {
+      console.error('Failed to create channel:', error);
+    }
+  };
+
+  const options = teamMembers.map((memberName) => ({
+    value: memberName,
+    label: memberName,
+  }));
+
+  const Option = (props) => {
     return (
-<form onSubmit={handleSubmitChannel} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-    <label>
-        Title:
-        <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-    </label>    
-    <ReactSelect
-  isMulti
-  options={options}
-  value={members.map(memberName => ({ value: memberName, label: memberName }))}
-  onChange={selectedOptions => setMembers(selectedOptions ? selectedOptions.map(option => option.value) : [])}
-  components={{ Option }}
-/>
-<label>
-        Private:
-        <input type="checkbox" onChange={e => setIsPrivate(e.target.checked)} />
-    </label>
-    <button type="submit">Create Channel</button>
-</form>
+      <div>
+        <components.Option {...props}>
+          {props.isSelected ? '✔' : ''} {props.label}
+        </components.Option>
+      </div>
     );
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmitChannel}
+      style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+    >
+      <label className="form-label">
+        Title:
+        <input
+          className="form-control"
+          type="text"
+          value={title}
+          placeholder='Your Channel title'
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+      <ReactSelect
+        isMulti
+        options={options}
+        value={members.map((memberName) => ({
+          value: memberName,
+          label: memberName,
+        }))}
+        onChange={(selectedOptions) =>
+          setMembers(
+            selectedOptions ? selectedOptions.map((option) => option.value) : []
+          )
+        }
+        components={{ Option }}
+      />
+      <div className="d-flex flex-column">
+        <label className="form-label m-1">
+          Private:
+          <input
+            className="m-1"
+            type="checkbox"
+            onChange={(e) => setIsPrivate(e.target.checked)}
+          />
+        </label>
+      </div>
+
+      <button className="btn btn-primary" type="submit">
+        Create Channel
+      </button>
+    </form>
+  );
 }
 
 CreateChannel.propTypes = {
-    teamId: PropTypes.string,
-    owner: PropTypes.shape({
-      id: PropTypes.string,
-      name: PropTypes.string,     
-    })
+  teamId: PropTypes.string,
+  owner: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
 };
--- main/src/views/ErrorPage/ErrorPage.jsx
+++ main/src/views/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,15 @@
-export default function ErrorPage() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBomb } from '@fortawesome/free-solid-svg-icons';
 
-    return (
-        <div>
-            <h1>Ooops! Page not found!</h1>
-        </div>
-    )
+export default function ErrorPage() {
+  return (
+    <div
+      className="container rounded .bg-secondary-subtle
+      align-items-center text-black bg-light
+    justify-content-center px-3 py-4 text-center mt-5"
+    >
+      <h1 className="align-self-center mb-5">Ooops! Page not found!</h1>
+      <FontAwesomeIcon icon={faBomb} size='5x'/>
+    </div>
+  );
 }
